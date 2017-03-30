2317f26fbc17ca02bd21a4eae05997b1234bd0d7
Jim O <Jimbook@Jims-Air.fios-router.home>
2017-03-29 23:55:55 -0400

    1

--- ui.R
+++ ui.R
@@ -6,7 +6,7 @@ ui <- bootstrapPage(
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
   leafletOutput("map", width = "100%", height = "100%"),
   absolutePanel(top = 10, right = 10,
-                sliderInput("range", "Magnitudes", min(quakes$mag), max(quakes$mag),
+                sliderInput("range", "Inicidents", min(quakes$mag), max(quakes$mag),
                             value = range(quakes$mag), step = 0.1
                 ),
                 selectInput("colors", "Color Scheme",
